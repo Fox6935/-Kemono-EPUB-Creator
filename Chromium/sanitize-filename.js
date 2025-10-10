85159b66f23adb63bbd74fc9cfbeab25af667f4f
Fox6935 <tysonhofer@hotmail.com>
2025-10-10 16:19:17 -0500

    Delete Chromium/sanitize-filename.js
    delete unused file

--- Chromium/sanitize-filename.js
+++
@@ -1,7 +0,0 @@
-// sanitize-filename.js
-export default function sanitizeFilename(filename) {
-  if (typeof filename !== 'string') {
-    return '';
-  }
-  return filename.replace(/[/\\?%*:|"<>]/g, '_');
-}
